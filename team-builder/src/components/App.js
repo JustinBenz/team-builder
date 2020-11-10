9733857b00d5fb554d834e27c2b63bef6cb65bf5
Justin Benz <jbenz156@gmail.com>
2020-11-10 18:47:02 -0500

    got data transferred through form to two different states

--- team-builder/src/components/App.js
+++ team-builder/src/components/App.js
@@ -10,12 +10,23 @@ const initialTeamList = {
 
 function App() {
   const updateForm = (inputName, inputValue) => {
-
+    setTeamList({...teamList, [inputName]: inputValue})
+  }
+  const submit = () => {
+    const newMember = {
+      name: teamList.name.trim(),
+      email: teamList.email.trim(),
+      role: teamList.role.trim(),
+    }
+    setTeamMembers([...teamMembers, newMember])
+    setTeamList(initialTeamList);
   }
   const[teamList, setTeamList]= useState(initialTeamList);
+  const [teamMembers, setTeamMembers] = useState([])
+
   
   return (
-    <Form data={teamList} update={updateForm}/>
+    <Form data={teamList} update={updateForm} submit={submit}/>
   );
 }
 
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,25 +1,31 @@
 import React from "react"
 import '../style/App.css'
+import './App'
 export default function Form (props) {
     const { data, update, submit } = props;
     const changeHandle = (evt) => {
       const { name, value } = evt.target;
+      update(name, value);
+    }
+    const submitHandle = (evt) => {
+        evt.preventDefault();
+        submit();
     }
     return(
         <div className="form">
             <div className="formholdr">
-                <label for="fName">Name:</label>
+                <label htmlFor="fName">Name:</label>
                 <input type="text" name="name" value={data.name} onChange={changeHandle} id="fName"></input>
-                <label for="fEmail">Email:</label>
+                <label htmlFor="fEmail">Email:</label>
                 <input type="email" name="email" value={data.email} onChange={changeHandle} id="fEmail"></input>
-                <label for="fDD" name="role">Role:</label>
+                <label htmlFor="fDD" name="role">Role:</label>
                 <select name="role" id="fDD" value={data.role} onChange={changeHandle}>
                     <option value="">------</option>
-                    <option value="Backend Engineer">Backend Engineer</option>
-                    <option value="Frontend Engineer">Frontend Engineer</option>
+                    <option value="Backend-Engineer">Backend Engineer</option>
+                    <option value="Frontend-Engineer">Frontend Engineer</option>
                     <option value="Designer">Designer</option>
                 </select>
-                <button>Submit</button>
+                <button onClick={submitHandle}>Submit</button>
             </div>
             
         </div>
